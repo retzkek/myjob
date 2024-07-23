ca4b2b2f5e230447722ae0bacc3a482384f2dbe6
Kevin Retzke <kretzke@fnal.gov>
2024-07-23 15:09:35 -0400

    add request logging

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rogpeppe/go-internal v1.9.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/urfave/cli/v2 v2.27.2 // indirect
 	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
 	github.com/xrash/smetrics v0.0.0-20240312152122-5f08fbb34913 // indirect
--- main.go
+++ main.go
@@ -6,8 +6,11 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
+	"time"
 
 	"github.com/retzkek/myjob/pkg/lens"
+	log "github.com/sirupsen/logrus"
 )
 
 var (
@@ -17,17 +20,22 @@ var (
 func main() {
 	flag.Parse()
 
-	http.HandleFunc("/status/{jobid}", func(w http.ResponseWriter, r *http.Request) {
-		if err := JobStatus(r.Context(), r.PathValue("jobid"), w); err != nil {
-			http.Error(w, err.Error(), 503)
-		}
-	})
+	statusHandler := JobStatus{}
+	http.Handle("/status/{jobid}", loggingHandler(statusHandler))
 
 	fmt.Println("Listening on", *address)
 	http.ListenAndServe(*address, nil)
 }
 
-func JobStatus(ctx context.Context, jobid string, w io.Writer) error {
+type JobStatus struct{}
+
+func (s JobStatus) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if err := s.getStatus(r.Context(), r.PathValue("jobid"), w); err != nil {
+		http.Error(w, err.Error(), 500)
+	}
+}
+
+func (s JobStatus) getStatus(ctx context.Context, jobid string, w io.Writer) error {
 	j, err := lens.GetJobInfo(ctx, jobid)
 	if err != nil {
 		return err
@@ -39,4 +47,41 @@ func JobStatus(ctx context.Context, jobid string, w io.Writer) error {
 	}
 	fmt.Fprintf(w, "Subission %s submitted by %s at %s is %s.\n", jobid, j.Owner, j.SubmitTime.String(), done)
 	return nil
+}
+
+// loggingHandler wraps an http.Handler to log each request
+func loggingHandler(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		path := r.URL.EscapedPath()
+		var mpath string
+		switch {
+		case strings.HasPrefix(path, "/status"):
+			mpath = "/status"
+		default:
+			mpath = "other"
+		}
+		h.ServeHTTP(w, r)
+
+		// log completed request
+		d := time.Since(start)
+		log.WithFields(log.Fields{
+			"origin":      originAddr(r),
+			"length":      r.ContentLength,
+			"path":        mpath,
+			"method":      r.Method,
+			"duration_ns": d.Nanoseconds(),
+			"duration":    d.String(),
+		}).Info("handled request")
+	})
+}
+
+// originAddr returns the "real" remote address for forwarded requests
+func originAddr(r *http.Request) string {
+	if remote := r.Header.Get("X-Real-IP"); remote != "" {
+		return remote
+	} else if remote := r.Header.Get("X-Forwarded-For"); remote != "" {
+		return remote
+	}
+	return r.RemoteAddr
 }
