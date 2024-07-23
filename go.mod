bdfff340afee3969d359c66313c8ca4cd764583a
Kevin Retzke <kretzke@fnal.gov>
2024-07-23 16:17:33 -0400

    add request tracing

--- go.mod
+++ go.mod
@@ -4,10 +4,15 @@ go 1.22.5
 
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/clok/kemba v1.2.1 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/gookit/color v1.5.4 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.20.0 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/machinebox/graphql v0.2.2 // indirect
@@ -17,12 +22,24 @@ require (
 	github.com/prometheus/client_model v0.5.0 // indirect
 	github.com/prometheus/common v0.48.0 // indirect
 	github.com/prometheus/procfs v0.12.0 // indirect
-	github.com/rogpeppe/go-internal v1.10.0 // indirect
+	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/urfave/cli/v2 v2.27.2 // indirect
 	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
 	github.com/xrash/smetrics v0.0.0-20240312152122-5f08fbb34913 // indirect
-	golang.org/x/sys v0.17.0 // indirect
-	google.golang.org/protobuf v1.33.0 // indirect
+	go.opentelemetry.io/otel v1.28.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.28.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.28.0 // indirect
+	go.opentelemetry.io/otel/metric v1.28.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.28.0 // indirect
+	go.opentelemetry.io/otel/trace v1.28.0 // indirect
+	go.opentelemetry.io/proto/otlp v1.3.1 // indirect
+	golang.org/x/net v0.26.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240701130421-f6361c86f094 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240701130421-f6361c86f094 // indirect
+	google.golang.org/grpc v1.64.0 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
 )
--- main.go
+++ main.go
@@ -13,19 +13,52 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/retzkek/myjob/pkg/lens"
 	log "github.com/sirupsen/logrus"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
+	"go.opentelemetry.io/otel/trace"
 )
 
 var (
-	address = flag.String("a", "localhost:8888", "Address and port to listen on")
+	address     = flag.String("a", "localhost:8888", "Address and port to listen on")
+	serviceName = semconv.ServiceNameKey.String("myjob")
 )
 
 func main() {
 	flag.Parse()
 
-	statusHandler := JobStatus{}
-	http.Handle("/status/{jobid}", loggingHandler(statusHandler))
+	ctx := context.Background()
 
-	http.Handle("/metrics", loggingHandler(promhttp.Handler()))
+	res, err := resource.New(ctx,
+		resource.WithAttributes(
+			// The service name used to display traces in backends
+			serviceName,
+		),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	shutdownTracerProvider, err := initTracerProvider(ctx, res)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := shutdownTracerProvider(ctx); err != nil {
+			log.Fatalf("failed to shutdown TracerProvider: %s", err)
+		}
+	}()
+
+	name := "github.com/retzkek/myjob"
+	tracer := otel.Tracer(name)
+
+	statusHandler := JobStatus{}
+	http.Handle("/status/{jobid}", loggingHandler(statusHandler, tracer))
+
+	http.Handle("/metrics", loggingHandler(promhttp.Handler(), tracer))
 
 	fmt.Println("Listening on", *address)
 	http.ListenAndServe(*address, nil)
@@ -68,8 +101,33 @@ func init() {
 	prometheus.MustRegister(requestDuration)
 }
 
+// Initializes an OTLP exporter, and configures the corresponding trace provider.
+func initTracerProvider(ctx context.Context, res *resource.Resource) (func(context.Context) error, error) {
+	// Set up a trace exporter
+	traceExporter, err := otlptracehttp.New(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create trace exporter: %w", err)
+	}
+
+	// Register the trace exporter with a TracerProvider, using a batch
+	// span processor to aggregate spans before export.
+	bsp := sdktrace.NewBatchSpanProcessor(traceExporter)
+	tracerProvider := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithResource(res),
+		sdktrace.WithSpanProcessor(bsp),
+	)
+	otel.SetTracerProvider(tracerProvider)
+
+	// Set global propagator to tracecontext (the default is no-op).
+	otel.SetTextMapPropagator(propagation.TraceContext{})
+
+	// Shutdown will flush any remaining spans and shut down the exporter.
+	return tracerProvider.Shutdown, nil
+}
+
 // loggingHandler wraps an http.Handler to log each request
-func loggingHandler(h http.Handler) http.Handler {
+func loggingHandler(h http.Handler, t trace.Tracer) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 		path := r.URL.EscapedPath()
@@ -82,6 +140,11 @@ func loggingHandler(h http.Handler) http.Handler {
 		default:
 			mpath = "other"
 		}
+		// start tracing span
+		// TODO extract from headers if possible
+		ctx, span := t.Start(r.Context(), mpath)
+		defer span.End()
+		r = r.WithContext(ctx)
 		h.ServeHTTP(w, r)
 
 		// log completed request
@@ -93,6 +156,7 @@ func loggingHandler(h http.Handler) http.Handler {
 			"method":      r.Method,
 			"duration_ns": d.Nanoseconds(),
 			"duration":    d.String(),
+			"traceid":     span.SpanContext().TraceID().String(),
 		}).Info("handled request")
 		requestDuration.WithLabelValues(mpath).Observe(d.Seconds())
 	})
